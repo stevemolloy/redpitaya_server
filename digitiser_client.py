a33bed2402818e70da542b7c2472222ef1ee0e06
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2020-04-23 14:09:50 +0000

    Added the client

---
+++ digitiser_client.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from socket import socket, AF_INET, SOCK_STREAM
+from numpy import frombuffer
+
+CONNECTION  = ('127.0.0.1', 65432)
+
+def get_all_data(sock):
+    all_data = []
+    data = sock.recv(4096)
+    while data:
+        all_data.append(data)
+        data = sock.recv(4096)
+    return all_data
+
+def digitiser_acquire(channel):
+    if not (channel==0 or channel==1):
+        raise ValueError('channel must be 0 or 1')
+    with socket(AF_INET, SOCK_STREAM) as s:
+        s.connect(CONNECTION)
+        s.sendall(b'digi' + b':' + str(channel).encode())
+        all_data = get_all_data(s)
+
+    # Convert binary data in buffer to an array of float32's
+    return frombuffer(b''.join(all_data), dtype='float32')
+    
+def atten_set(val):
+    with socket(AF_INET, SOCK_STREAM) as s:
+        s.connect(CONNECTION)
+        s.sendall(b'atten' + b':' + str(val).encode())
+        data = s.recv(1024)
+    
+    return float(data)
+
