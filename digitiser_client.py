7436de8186719e25f1ee49ca77d90ac260f1d98c
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2020-04-28 12:41:23 +0200

    Send the connection data

--- digitiser_client.py
+++ digitiser_client.py
@@ -13,7 +13,7 @@ def get_all_data(sock):
         data = sock.recv(4096)
     return all_data
 
-def digitiser_acquire(channel):
+def digitiser_acquire(channel, CONNECTION=CONNECTION):
     if not (channel==0 or channel==1):
         raise ValueError('channel must be 0 or 1')
     with socket(AF_INET, SOCK_STREAM) as s:
@@ -24,7 +24,7 @@ def digitiser_acquire(channel):
     # Convert binary data in buffer to an array of float32's
     return frombuffer(b''.join(all_data), dtype='float32')
     
-def atten_set(val):
+def atten_set(val, CONNECTION=CONNECTION):
     with socket(AF_INET, SOCK_STREAM) as s:
         s.connect(CONNECTION)
         s.sendall(b'atten' + b':' + str(val).encode())
